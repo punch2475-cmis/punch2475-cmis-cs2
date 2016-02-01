5e3b3865105bb34f2e29cbce8783142528662b6b
cmis <cmis@cmis-hs22.(none)>
2016-02-01 13:34:43 +0700

    finish until print1

---
+++ assignment1.py
@@ -0,0 +1,24 @@
+myName= "Punch"
+myAge = 15.25
+myHeight = 1.64
+sqLength = 25
+recLength = 77
+recHeight = 36
+ageMonth = myAge * 12
+yearsLeft = 78.8-myAge
+heightFeet = myHeight*3.28084
+avgHeight = myHeight-1.57
+areaSquare = sqLength**2
+volumeCube = (areaSquare**2)/2
+areaRec = (recHeight*recLength)/9
+print volumeCube
+print areaRec
+print areaSquare
+print heightFeet
+print avgHeight
+print "My height in meters is " + str(myHeight) + ". Therefore, my height in feet is " + str(heightFeet) +"My name is  " + str(myName) +"My remaining years on earth is " + str(yearsLeft) +"I'm"+ str(myAge) + " and 1/9 of the volume is " + str(areaRec) +"."
+
+
+
+
+
