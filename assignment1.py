1028560fc5d034a857031ff03d3c59f6abf453b4
cmis <cmis@cmis-hs22.(none)>
2016-02-03 08:23:50 +0700

    finish

--- assignment1.py
+++ assignment1.py
@@ -1,22 +1,25 @@
-myName= "Punch"
-myAge = 15.25
-myHeight = 1.64
-sqLength = 25
-recLength = 77
-recHeight = 36
-ageMonth = myAge * 12
-yearsLeft = 78.8-myAge
-heightFeet = myHeight*3.28084
-avgHeight = myHeight-1.57
-areaSquare = sqLength**2
-volumeCube = (areaSquare**2)/2
-areaRec = (recHeight*recLength)/9
-print volumeCube
-print areaRec
-print areaSquare
-print heightFeet
-print avgHeight
-print "My height in meters is " + str(myHeight) + ". Therefore, my height in feet is " + str(heightFeet) +"My name is  " + str(myName) +"My remaining years on earth is " + str(yearsLeft) +"I'm"+ str(myAge) + " and 1/9 of the volume is " + str(areaRec) +"."
+myName= "Punch" #my name
+myAge = 15.25   #my age in year
+myHeight = 1.64 #my height in meters 
+sqLength = 25  #square length in centimeter
+recLength = 77 #rectangle lenght in centimeter
+recHeight = 36 #rectangle height in centimeter
+ageMonth = myAge * 12 #age in month 
+yearsLeft = 78.8-myAge #years left on earth 
+heightFeet = myHeight*3.28084 #height in feet
+avgHeight = myHeight-1.57 #average height compare with my height
+areaSquare = sqLength**2 #area of square 
+volumeCube = (sqLength**3)/2 #half of the volume cube
+areaRec = (recHeight*recLength)/9 #1/9 of area of the rectangle
+wSmiley = ";)" #winking smiley face
+print volumeCube #print half of the volume cube
+print areaRec #print area of rectangle
+print areaSquare #print area of square
+print heightFeet #print height in feet
+print avgHeight #print average height 
+print "My height in meters is " + str(myHeight) + "m. So, my height in feet is " + str(heightFeet) +"feet. My name is " + str(myName) +". My remaining years on earth is " + str(yearsLeft) +"years. I'm "+ str(myAge) + "years old  and my age in month is " + str(ageMonth) +"." #print message using string operator (+)
+print "Hello, My name is",myName, "and I'm" ,myAge, "years old." "My rectangle length is",recLength,"cm" " and my rectangle height is", recHeight,"cm" " ,so 1/9 of my rectangle area is",areaRec,"cm square." #print message using comma to print multiple values
+print wSmiley*10000 #print 10000 winking smiley face
 
 
 
