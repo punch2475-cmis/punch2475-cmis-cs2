ae345566fa1860101d3dc6a44abaaffdbd2c569b
cmis <cmis@cmis-hs22.(none)>
2016-02-09 08:41:22 +0700

    finish add...

---
+++ functions.py
@@ -0,0 +1,26 @@
+import math
+def add(a,b):
+	return a+b
+
+print add(3,4)
+
+def sub(a,b):
+	return a-b
+
+print sub(5,3)
+
+def mul(a,b):
+	return a*b
+
+print mul(4,4)
+
+def div(a,b):
+	return a/b
+
+print div(2,3.0)
+
+def divhrs(a,b):
+	b=60
+	return a/b
+
+print 86400/b
