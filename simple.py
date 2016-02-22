3a7ff6152e5df71a6edbc155fba1dbe756be7e58
punch2475-cmis <punch2475@cmis.ac.th>
2016-02-22 16:12:57 +0700

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,20 @@
+import math
+def output(hyp,sleg,lleg):
+	out="""
+Your
+
+
+
+""".format(hyp,sleg,lleg)
+	return out 
+
+
+def main():
+	hyp=raw_input("Your hypotheuse length")
+	sleg=raw_input("Your short leg length")
+	lleg=raw_input("Your long leg length")
+	
+
+
+def sine(oppleg,hyp):
+	return oppleg/hyp
