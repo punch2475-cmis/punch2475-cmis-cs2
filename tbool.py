bb44e78546344c3ba77a6e8a1524ed81d921addd
cmis <cmis@cmis-hs19.(none)>
2016-03-01 10:53:50 +0700

    try

---
+++ tbool.py
@@ -0,0 +1,17 @@
+def Happy():
+    print ":)"
+
+def Sad():
+    print ":("
+
+Yourehappy=raw_input("Are you happy?")
+YoureSad=raw_input ("Are you sad?")
+
+True=Happy()
+False=Sad()
+
+if Yourehappy and YoureSad:
+   
+
+print "Bye"
+
