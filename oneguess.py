c76327d5c725ecf651b293727bf3a197deeba8a7
cmis <cmis@cmis-hs01.(none)>
2016-03-17 08:42:15 +0700

    Done

--- oneguess.py
+++ oneguess.py
@@ -1,31 +1,34 @@
 import random
 
-def guess():
-	import random
-	minimum_num = int(raw_input("What is the minimum value?"))
-	maximum_num = int(raw_input("What is the maximum value?"))
-	rd = random.randint(minimum, maximum)
-	print "I am thinking of a number between", minimum, "and", maximum
-	guess = int(raw_input("What do you think it is?"))
-	difference = abs(guess - rd)
+def main():
+    mininumber= int(raw_input("What is the minimum number?"))
+    maxnumber= int(raw_input ("What is the maximum number?"))
+    print "I am thinking of a number between", mininumber, "and", maxnumber, "." 
+    guess = int(raw_input("What do you think it is ?"))
+    number_range = random.randint(mininumber, maxnumber)
+    difference = abs(guess-number_range)
+    out= output(number_range,guess,difference)
 
-	if guess == rd:
-		print """
-The target was {}
-Your guess was {}
+def output(number_range,guess,difference): 
+    if guess == number_range:
+        print """
+The target was {}.
+Your guess was {}.
 That is correct! You must be a phychic!
-""".format(random, guessnumber)
-	elif guess > rd:
-		print """
-The target was {}
-Your guess was {}
-That's over by {}
-""".format(random, guessnumber, difference)
-	elif guessnumber < random:
-		print """
-The target was {}
-Your guess was {}
-That's under by {}
-""".format(random, guessnumber, difference)
-	
-guess()
+""". format(number_range,guess)
+
+    elif guess > number_range:
+        print """
+The target was {}.
+Your guess was {}.
+That's over by {}.
+""". format(number_range,guess, difference)
+
+    elif guess < number_range:
+        print """
+The target was {}.
+Your guess was {}.
+That's under by {}.
+""". format(number_range,guess,difference) 
+
+main()
