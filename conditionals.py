f61553e326d12a64957ca000cdd63b024007e0fd
cmis <cmis@cmis-hs01.(none)>
2016-03-29 13:35:38 +0700

    Nearly done

--- conditionals.py
+++ conditionals.py
@@ -1,11 +1,29 @@
-#Future job
+#Future major and GPA
+def cal:
+    return 
 
 def main():
     name= raw_input ("What is your name?")
     age= raw_input("How old are you?")
     grade= raw_input ("Grade:")
-    sumOfeverything= subject + studyyears + reading + schoolgrade
+    sumOfeverything= cal(studyyears,reading,schoolgrade,homework,character)
     
+def GPA():
+    if sumOfeverything >= 6:
+        print "2.0"
+    if sumOfeverything >= 7 <= 12:
+        print "2.4 - 2.9"
+    if sumOfeverything >= 13 <= 19:
+        print "3.0-3.4"
+    if sumOfeverything >= 20 <= 25:
+        print "3.5-4.0"
+    return GPA
+
+def randomGPA():
+    rdGPA = random.randint(2.0,4.0)
+    rGPA = float(rdGPA+ GPA)/2
+
+
 def subject():
     subject= ("""
 What subject do you like?
@@ -17,17 +35,17 @@ e. english
 f. economic 
 (type a,b,c,d,e,f):
                """)
-    if subject== "a" 
+    if subject== "a":
         return 1
-    if subject== "b" 
+    if subject== "b":
         return 2
-    if subject== "c"
+    if subject== "c":
         return 3
-    if subject== "d"
+    if subject== "d":
         return 4
-    if subject== "e"
+    if subject== "e":
         return 5
-    if subject== "f"
+    if subject== "f":
         return 6
 
 def studyyears():
@@ -40,15 +58,15 @@ d. 5
 e. 6
 (type a,b,c,d,e):
                """)
-    if studyyears== "a"
+    if studyyears== "a":
         return 1
-    if studyyears== "b"
+    if studyyears== "b":
         return 2
-    if studyyears== "c"
+    if studyyears== "c":
         return 3
-    if studyyears== "d"
+    if studyyears== "d":
         return 4
-    if studyyears== "e"
+    if studyyears== "e":
         return 5
 
 def reading():
@@ -59,11 +77,11 @@ b. It's okay
 c. I love it
 (type a,b,c):
             """)
-    if reading== "a"
+    if reading== "a":
         return 1
-    if reading== "b"
+    if reading== "b":
         return 2
-    if reading== "c"
+    if reading== "c":
         return 3
 
 def schoolgrade():
@@ -75,23 +93,62 @@ c. C
 e. A-B
 (type a,b,c,e):
             """)
-    if schoolgrade == "a"
+    if schoolgrade == "a":
         return 1
-    if schoolgrade == "b"
+    if schoolgrade == "b":
         return 2
-    if schoolgrade == "c"
+    if schoolgrade == "c":
         return 3
-    if schoolgrade == "e"
+    if schoolgrade == "e":
         return 4
-def homework():
 
+def homework():
+    homework("""
+How often did you finish homework?
+a. Never
+b. Rarely
+c. Always
+(type a,b,c):
+            """)
+    if homework == "a":
+        return 1
+    if homework == "b": 
+        return 2
+    if homework == "c":
+        return 3
+
+def character():
+    character("""
+Which of these character best suit for you?
+a. I'm not organized
+b. I'm very organized
+(type a,b):
+        """)
+    if character == "a":
+        return 1
+    if character == "b":
+        return 2
+def job ():
+    if sumOfeverthing >= 10 
+        print "you can study in major __________"
+    if sumOfeverything >= 11 <= 19
+        print "you can study in major __________"
+    if sumOfeverything >= 20 <= 24
+        print "you can study in major __________"
 
 def output():
-	out="""
-Hello {}, you are {} years old. You are in grade {}.
-From the quiz that you take, your future job is going to be {}.
+	output="""
+Hello {}, you are {} years old. You are in grade {}. 
+From the quiz that you take, your future job is going to be {}. Also you will graduate with GPA {}. 
 """. format(name,age,grade)
-    return out
+   if sumOfeverything <= 10:
+        print """
+Hello {}, you are {} years old.
+You are in grade {}.
+From the quiz you take above, your future job will be in {}. 
+Also you will graduate with GPA {}. 
+""". format(_jsgjlks;lgj.__gklsjgl.__.)
 
+main ()
 
     
