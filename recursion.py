53f4405b911ba19de894dc9da3e40b66d41b202e
cmis <cmis@cmis-hs01.(none)>
2016-04-22 13:34:39 +0700

    NOT DONE T_T

--- recursion.py
+++ recursion.py
@@ -1,12 +1,26 @@
-def countup(n):
-	if n>=10:
-		print "Blastoff"
-	else:
-		print n
-		countup(n+1)
+def countdown_from_to(start,stop):
+    if start<stop:
+        print "Blastoff"
+    else:
+        print start
+        countdown_from_to(start-1,stop)
+
+def countup_from_to(start,stop):
+    if start>stop:
+        print "Blastoff"
+    else:
+        print start
+        countup_from_to(start+1,stop)
+        
+def adder():
+    print "Running Total:0"
+    b= raw_input("Next Number:")
+
+    
 
 def main():
-	countup(-145326)
+    adder()
+    return
 
 main()
 	
