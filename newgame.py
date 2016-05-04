107824b5c324af332f967a582835e2400ff6a576
cmis <cmis@cmis-hs01.(none)>
2016-05-04 13:34:48 +0700

    not done

--- newgame.py
+++ newgame.py
@@ -1,10 +1,15 @@
 import random 
 
-def main():
-
-
-
-main()
-
 def guess():
+	n= raw_input("Number:")
+	target= random.randint(1,100)
 	
+	print target
+
+	if n == target:
+		print "good"
+	if n < target:
+		print "too low"
+	if n > target:
+		print "too high"
+guess()
