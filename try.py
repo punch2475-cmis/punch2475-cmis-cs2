2a8dda747462464580fcb85bda87d3c0fc09ab20
cmis <cmis@cmis-hs07.(none)>
2016-05-24 13:35:22 +0700

    ...

---
+++ try.py
@@ -0,0 +1,30 @@
+
+while False:
+	print ":("
+
+#x=10
+#while x >=0:
+#	print x
+#	x -=1 
+def countdown(x):
+	while x >=0:
+		print x
+		x-=1
+
+def countup(x):
+	while x<10:
+		print x
+		x+=1
+def count(x):
+	while x < 0:
+		print x
+		x+=1
+	while x >0:
+		print x
+		x-=1
+	while x==0:
+		print x
+def main():
+	count(9)
+
+main()
