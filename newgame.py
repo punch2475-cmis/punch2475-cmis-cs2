bad5cf5a101128fdc77f6d23288baae4aecf602a
punch2475-cmis <punch2475@cmis.ac.th>
2016-05-26 06:27:24 +0700

    Add files via upload

--- newgame.py
+++ newgame.py
@@ -1,28 +1,56 @@
-import random 
+import random
 
-def guess():
-	n= raw_input("Number:")
-	target= random.randint(1,100)
-	p= int(n)
-	
-	print target
-
-	if p == target:
-		print "good"
-	if p < target :
-		print "too low"
-	if p > target:
-		print "too high"
-	guess()
-
-def number_round():
-	if number_round == 0:
-		return "Out of round"
-	else:
-		return guess()
+def guess1(tries, target):
+    if tries == 5:
+        target = random.randint(0,100)
+    if tries == 0:
+        print "You suck at this"
+        return False
+    else:
+        guess = float(raw_input("Guess a number: "))
+        if guess == target:
+            print "Correct!"
+        elif guess < target:
+            print "Too low"
+            return guess1(tries-1, target)
+        else:
+            print "Too high"
+            return guess1(tries-1, target)
+def guess2(tries):
+        random3= random.randint(0,100)
+        if tries >=1:
+                print "I guess {}." .format(random3)
+                result= raw_input("(H)igh, (L)ow, (C)orrect")
+                if result == "C":
+                    print "Awesome"
+                elif result == "H":
+                    print "Too high"
+                    return guess2(tries -1)
+                else:
+                    print "Too low"
+                    return guess2(tries -1)
+                if tries == 0:
+                    print "T_T"
+                else: 
+                    guess2(tries-1)
+            
+def rounds(number_rounds,tries,target):
+    if number_rounds == 0:
+        print "Done"
+    else:
+        number_rounds = 2
+        print "You have", (number_rounds-1),"round left"
+        return guess1(tries-1, target) 
 
 def main():
-	number_round = 3
-	number_tries = 5
-	
+    tries= 5
+    target=0
+    number_rounds=3
+    guess1(tries,target)
+    guess2(tries)
+    rounds(number_rounds,tries,target)
+
+main()
+
+
 
