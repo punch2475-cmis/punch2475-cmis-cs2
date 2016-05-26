4931f0baa5d503c2d00b31cd9805abaec90ca38e
cmis <cmis@cmis-hs07.(none)>
2016-05-26 08:38:23 +0700

    T_T

--- try.py
+++ try.py
@@ -22,9 +22,24 @@ def count(x):
 	while x >0:
 		print x
 		x-=1
-	while x==0:
-		print x
+def countFrom2(x,y):
+	print x
+	while x > y:
+		print x-1
+		x-=1
+	while x < y:
+		print x+1
+		x+=1
+def addOdd(n):
+	if n >0:
+		while n%2== 1:
+		sum+= n
+		print n
+	if n <0:
+		while n% 2 ==0:
+		return 0
+ 
 def main():
-	count(9)
+	addOdd(3)
 
 main()
