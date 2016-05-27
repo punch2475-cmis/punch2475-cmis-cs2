d357dc3bef03ea9530f00c176647bd91b8555eaa
cmis <cmis@cmis-hs07.(none)>
2016-05-27 10:54:52 +0700

    NT

--- try.py
+++ try.py
@@ -31,15 +31,32 @@ def countFrom2(x,y):
 		print x+1
 		x+=1
 def addOdd(n):
+	sum = 0
 	if n >0:
-		while n%2== 1:
-		sum+= n
-		print n
-	if n <0:
-		while n% 2 ==0:
-		return 0
- 
-def main():
-	addOdd(3)
+		while n > 0:
+			if n % 2 ==1:
+				sum+= n
+			n-=1
+	elif n <0:
+		while n > 0:
+			if n % 2 ==1:
+				sum+= n
+			n+=1
+	return sum
 
-main()
+def grid(w,h):
+	out =""
+	x = 0
+	while x < w:
+		out += "."
+		x+=1
+	while x < h:
+		out += "."
+		x+=1
+	return out
+	
+print grid (10,5)
+	
+
+
+
